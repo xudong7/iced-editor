ac0c6810962296b3cfbf26204076a0dd2021232b
蹲家 <13433126742@163.com>
2025-04-07 21:13:26 +0800

    完善编辑器功能，添加自定义字体支持及图标按钮逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use iced::widget::{button, column, container, horizontal_space, row, text, text_editor};
-use iced::{Application, Command, Element, Length, Settings, Theme, executor};
+use iced::{Application, Command, Element, Font, Length, Settings, Theme, executor};
 
 use std::io;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 fn main() -> iced::Result {
-    Editor::run(Settings::default())
+    Editor::run(Settings {
+        fonts: vec![
+            include_bytes!("../fonts/editor-icons.ttf")
+                .as_slice()
+                .into(),
+        ],
+        ..Settings::default()
+    })
 }
 
 struct Editor {
@@ -90,10 +97,12 @@ impl Application for Editor {
 
     fn view(&self) -> Element<'_, Message> {
         let controls = row![
-            button("New").on_press(Message::New),
-            button("Open").on_press(Message::Open),
-            button("Save").on_press(Message::Save),
-        ];
+            action(new_icon(), Message::New),
+            action(open_icon(), Message::Open),
+            action(save_icon(), Message::Save),
+        ]
+        .spacing(10);
+
         // text input
         let input = text_editor(&self.content).on_edit(Message::Edit);
 
@@ -127,6 +136,31 @@ impl Application for Editor {
     fn theme(&self) -> Theme {
         Theme::Dark
     }
+}
+
+fn action<'a>(content: Element<'a, Message>, on_press: Message) -> Element<'a, Message> {
+    button(container(content).width(30).center_x())
+        .on_press(on_press)
+        .padding([5, 10])
+        .into()
+}
+
+fn new_icon<'a>() -> Element<'a, Message> {
+    icon('\u{E800}')
+}
+
+fn open_icon<'a>() -> Element<'a, Message> {
+    icon('\u{F115}')
+}
+
+fn save_icon<'a>() -> Element<'a, Message> {
+    icon('\u{E801}')
+}
+
+fn icon<'a, Message>(codepoint: char) -> Element<'a, Message> {
+    const ICON_FONT: Font = Font::with_name("editor-icons");
+
+    text(codepoint).font(ICON_FONT).into()
 }
 
 fn default_path() -> PathBuf {
